215ce4f4493b9ec089ebedde4a44fa67729ed70f
alexi-mathioupollak <alexi.mathiou-pollak@etu.umontpellier.fr>
2021-09-30 09:06:20 +0200

    JS petite amélioration mais pas trop encore

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,18 @@
 $( function() {
-    $( "#draggable" ).draggable(
-        {
-            revert: true
-        }
-    );
+    $( "#draggable" ).draggable({revert: "invalid"}).sortable();
+    $( "#draggable2" ).draggable({revert: "invalid"}).sortable();
+    $( "#draggable3" ).draggable({revert: "invalid"}).sortable();
+    $( "#draggable" ).disableSelection();
+    $( "#droppable" ).droppable({
+        classes: {
+            "ui-droppable-active": "ui-state-active",
+            "ui-droppable-hover": "ui-state-hover",
+        },
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+            .html( "Dropped!" );
+      }
+    });
   } );
