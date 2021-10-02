3116f7fe16efb0092d65f6d572d387a04cf7f367
alexi-mathioupollak <alexi.mathiou-pollak@etu.umontpellier.fr>
2021-10-02 17:16:59 +0200

    Avancée

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,14 @@
-$( function() {
+/*$( function() {
     $( "#draggable" ).draggable({revert: "invalid"}).sortable();
     $( "#draggable2" ).draggable({revert: "invalid"}).sortable();
     $( "#draggable3" ).draggable({revert: "invalid"}).sortable();
     $( "#draggable" ).disableSelection();
-    $( "#droppable" ).droppable({
-        classes: {
-            "ui-droppable-active": "ui-state-active",
-            "ui-droppable-hover": "ui-state-hover",
-        },
-      drop: function( event, ui ) {
-        $( this )
-          .addClass( "ui-state-highlight" )
-          .find( "p" )
-            .html( "Dropped!" );
-      }
-    });
-  } );
+    $( "#droppable" ).droppable({classes: {"ui-droppable-active": "ui-state-active","ui-droppable-hover": "ui-state-hover",}});
+    function duplic(element,name)
+    {
+      clone = document.getElementById(element).cloneNode(true);
+      clone.name = name;
+    }
+     
+    duplic("element1","copie_element1");
+  } );*/
