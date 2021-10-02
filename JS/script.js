55a7ec068a5429ddda5b46d65df676aedfbb0619
alexi-mathioupollak <alexi.mathiou-pollak@etu.umontpellier.fr>
2021-10-02 22:05:40 +0200

    CSS avancée page GENERATOR.HTML

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,3 @@
-/*$( function() {
-    $( "#draggable" ).draggable({revert: "invalid"}).sortable();
-    $( "#draggable2" ).draggable({revert: "invalid"}).sortable();
-    $( "#draggable3" ).draggable({revert: "invalid"}).sortable();
-    $( "#draggable" ).disableSelection();
-    $( "#droppable" ).droppable({classes: {"ui-droppable-active": "ui-state-active","ui-droppable-hover": "ui-state-hover",}});
-    function duplic(element,name)
-    {
-      clone = document.getElementById(element).cloneNode(true);
-      clone.name = name;
-    }
-     
-    duplic("element1","copie_element1");
-  } );*/
+function onDragEvent(e) {
+    
+}
