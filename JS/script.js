af91159b32e0a2f5580ed43e67eaa76fa7d5a769
alexi-mathioupollak <alexi.mathiou-pollak@etu.umontpellier.fr>
2021-10-03 10:46:06 +0200

    JS + effects

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,49 @@
-function onDragEvent(e) {
-    
+const fill = document.querySelector('.b1');
+const fill2 = document.querySelector('.b2');
+const fill3 = document.querySelector('.b3');
+const empties = document.querySelectorAll('.form_list');
+
+// Fill listeners
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+fill2.addEventListener('dragstart', dragStart);
+fill2.addEventListener('dragend', dragEnd);
+fill3.addEventListener('dragstart', dragStart);
+fill3.addEventListener('dragend', dragEnd);
+
+// Loop through empty boxes and add listeners
+for (const empty of empties) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+
+// Drag Functions
+
+function dragStart() {
+    this.className += ' hold';
+    setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd() {
+    this.className = 'b1';
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    this.className += ' hovered';
+}
+
+function dragLeave() {
+    this.className = 'form_list';
+}
+
+function dragDrop() {
+    this.className = 'form_list';
+    this.append(fill);
 }
