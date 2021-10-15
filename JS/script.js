b776ed76e8f1f614229aa42da948e62c3e176f5a
alexi-mathioupollak <alexi.mathiou-pollak@etu.umontpellier.fr>
2021-10-15 14:52:25 +0200

    séparations CSS + début JS

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,8 @@
 const fill = document.querySelector('.b1');
 const fill2 = document.querySelector('.b2');
 const fill3 = document.querySelector('.b3');
-const empties = document.querySelectorAll('.form_list');
+const formlist = document.querySelectorAll('.form_list');
+const backlist = document.querySelectorAll('.receive_list');
 
 let fillDragged;
 
@@ -14,16 +15,27 @@ fill3.addEventListener('dragstart', dragStart);
 fill3.addEventListener('dragend', dragEnd);
 
 // Loop through empty boxes and add listeners
-for (const empty of empties) {
+for (const empty of formlist) {
     empty.addEventListener('dragover', dragOver);
     empty.addEventListener('dragenter', dragEnter);
-    empty.addEventListener('dragleave', dragLeave);
-    empty.addEventListener('drop', dragDrop);
+    empty.addEventListener('dragleave', dragLeaveBacklist);
+    empty.addEventListener('drop', dragDropToBackList);
+}
+
+for (const empty of backlist) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeaveFormList);
+    empty.addEventListener('drop', dragDropToFormList);
 }
 
 // Drag Functions
 
 function dragStart() {
+    if (this.parentNode.id == 'form_list_#') {
+        let copy = this.cloneNode(true);
+        this.parentNode.appendChild(copy);
+    }
     this.className += ' hold';
     setTimeout(() => (this.className = 'invisible'), 0);
     fillDragged = this;
@@ -42,11 +54,27 @@ function dragEnter(e) {
     this.className += ' hovered';
 }
 
-function dragLeave() {
+function dragLeaveFormList() {
+    console.log("je tire vers back");
     this.className = 'form_list';
 }
 
-function dragDrop() {
+function dragLeaveBacklist() {
+    console.log("je tire vers form");
+    this.className = 'receive_list';
+}
+// drop to drag
+function dragDropToBackList() {
     this.className = 'form_list';
-    this.append(fillDragged);
+    var box = document.getElementById(fillDragged.id);
+    box.parentNode.removeChild(box);
+    console.log("dragDropToBackList");
+}
+// drag to drop
+function dragDropToFormList() {
+    this.className = 'receive_list';
+    let copy = fillDragged;
+    this.appendChild(copy);
+    console.log("dragDropToFormList");
+    console.log(this.parentNode.id+ "ok");
 }
