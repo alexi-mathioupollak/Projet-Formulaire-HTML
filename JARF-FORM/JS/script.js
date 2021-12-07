ac9b765b39a395313756aaa8acf6e95471b5cb62
julien-renaud02 <julien.renaud02@etu.umontpellier.fr>
2021-12-07 22:04:58 +0100

    nouveau générateur

---
+++ JARF-FORM/JS/script.js
@@ -0,0 +1,36 @@
+// $('button').click(function(){
+//     $('body').css("background-color", "red");
+// })
+$(function () {
+    $("#items").accordion();
+});
+
+
+// drop un items de la liste au formulaire
+$(function () {
+    $(".drag").draggable({
+        cancel: false,
+        helper: 'clone',
+        revert: 'invalid'
+    });
+
+    $("#drop").droppable({
+        accept: ".drag",
+
+        drop: function (event, ui) {
+            $(this)
+                .append($(ui.draggable).clone()
+                    .removeClass("ui-draggable-dragging")
+                    .addClass('dropped')
+                    .removeClass("drag ui-draggable ui-draggable-handle ui-draggable-dragging")
+                );
+            // $("#code_generated").val($("#droppable").html())
+        }
+    });
+});
+
+// manipuler le formulaire (emplacement)
+$(function () {
+    $("#drop").sortable();
+});
+
